c851497b69f6bc68df2715aaefd4b0e417ec2033
Sébastien Velay <svelay@norauto.com>
2021-11-03 12:38:34 +0100

    spring boot

---
+++ src/test/java/com/zenika/tz/hexaarchi/HexaArchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.tz.hexaarchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaArchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
